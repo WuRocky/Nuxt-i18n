0ab7d9514a42cfc30c1b85a4cae462f4bf113c69
Rocky Wu <rocky_wu@everrich.com.tw>
2023-12-19 17:00:13 +0800

    i18n about 頁面更改

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center bg-white">
     <h1 class="mt-24 text-8xl font-medium text-blue-500">
-      {{ $t('hello') }}
+      {{ $t('about') }}
     </h1>
     <div class="my-8 flex flex-row justify-center">
       <label class="text-gray-600">{{ $t('language') }}</label>
